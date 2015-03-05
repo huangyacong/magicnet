9ce29316facda9dd5990f00eca0e25de107646be
聪聪 <huangyacong8@163.com>
2015-03-05 20:12:01 +0800

    整理加密解密代码

--- common/SeCrypt.h
+++ common/SeCrypt.h
@@ -1,6 +1,10 @@
 #ifndef SE_CRYPT_H
 #define SE_CRYPT_H
 
-void SeCrypt(const unsigned char *input, unsigned char *output, int length, const unsigned char *key, int keylen);
+// º”√‹
+void SeEnCrypt(const unsigned char *input, unsigned char *output, int length, const unsigned char *key, int keylen);
+
+// Ω‚√‹
+void SeDeCrypt(const unsigned char *input, unsigned char *output, int length, const unsigned char *key, int keylen);
 
 #endif
