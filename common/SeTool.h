2031fe683e319cd1df26bfaabfb9cf4f1e554e46
huangyacong <huangyacong8@163.com>
2015-03-12 14:45:23 +0800

    整理

--- common/SeTool.h
+++ common/SeTool.h
@@ -14,25 +14,6 @@
 #include <stdio.h>
 #include <time.h>
 
-// mysql: datetime->'1971-01-01 00:00:00'to'9999-12-31 23:59:59'; TIMESTAMP->1970to2037
-// valid time in '1971-01-01 00:00:00'->'9999-12-31 23:59:59'
-
-// now time
-time_t newtime_t();
-
-// convert string time to time_t, pcTimeChar format to '9999-02-31 23:00:59'
-// if pcTimeChar error,return nowtime
-time_t string_to_time_t(const char* pcTimeChar);
-
-// return sec
-long long difftime_t(time_t timeEnd, time_t timeBegin);
-
-// add sec
-time_t addtime_t(time_t srcTime, int sec);
-
-// format to '9999-02-31 23:00:59', len >=20
-void formattime_t(time_t srcTime, char *pOut, int len);
-
 bool SeCHStrStr(const char* pcDstChar,const char* pcSrcChar);
 
 void SeStrNcpy(char* pcDstChar,int iDstLen,const char* pcSrcChar);
