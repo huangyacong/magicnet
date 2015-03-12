123d74662349c15924bfe0cdfba710761155258c
huangyacong <huangyacong8@163.com>
2015-03-12 14:55:49 +0800

    整理

--- common/SeThread.h
+++ common/SeThread.h
@@ -5,13 +5,10 @@
 
 #ifdef __linux
 
-#define _XOPEN_SOURCE
 #define _GNU_SOURCE
 #include <pthread.h>
 #include <unistd.h>
-#include <sys/time.h>
 #include <sched.h>
-#include <time.h>
 
 // compile the code with -lrt and -lpthread
 
