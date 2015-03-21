0fad4e79189cf280fcbc4d51b042d9cc09f25218
huangyacong <huangyacong8@163.com>
2015-03-21 17:47:44 +0800

    init net

--- magicnet/SeNetTcp.c
+++ magicnet/SeNetTcp.c
@@ -1,3 +1,25 @@
 #include "SeNetTcp.h"
 
+void SeNetTcpCreate(struct SENETTCP *pkNetTcp)
+{
+	int iBegin;
 
+	pkNetTcp->kHandle = SE_INVALID_HANDLE;
+	SeNetSreamInit(&pkNetTcp->kMemCache);
+	SeNetSSocketInit(&pkNetTcp->kSvrSocketList);
+	
+	SeNetCSocketInit(&pkNetTcp->kFreeCSocketList);
+	SeNetCSocketInit(&pkNetTcp->kActiveCSocketList);
+	SeNetCSocketInit(&pkNetTcp->kConnectCSocketList);
+	SeNetCSocketInit(&pkNetTcp->kDisConnectCSocketList);
+
+	for(iBegin = 0; iBegin < MAX_SOCKET_LEN; iBegin++) {
+		SeNetCSocketNodeInit(&pkNetTcp->kClientSocket[iBegin]);
+		pkNetTcp->kClientSocket[iBegin].iFlag = iBegin;
+		SeNetCSocketHeadAdd(&pkNetTcp->kFreeCSocketList, &pkNetTcp->kClientSocket[iBegin]);
+	}
+
+	pkNetTcp->pkOnConnectFunc = 0;
+	pkNetTcp->pkOnDisconnectFunc = 0;
+	pkNetTcp->pkOnRecvDataFunc = 0;
+}
