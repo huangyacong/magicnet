9d9faa2a6d1ec831691d045c380539cbc9c97472
huangyacong <huangyacong8@163.com>
2015-03-21 17:54:32 +0800

    init handle

--- magicnet/SeNetTcpForLinux.c
+++ magicnet/SeNetTcpForLinux.c
@@ -1,3 +1,14 @@
 #include "SeNetTcp.h"
 
+#if defined(__linux)
 
+void SeNetTcpInit(struct SENETTCP *pkNetTcp, SENETTCPONCONNECT pkOnConnectFunc, SENETTCPDISCONNECT pkOnDisconnectFunc, SENETTCPRECV pkOnRecvDataFunc)
+{
+	SeNetTcpCreate(pkNetTcp);
+	pkNetTcp->pkOnConnectFunc = pkOnConnectFunc;
+	pkNetTcp->pkOnDisconnectFunc = pkOnDisconnectFunc;
+	pkNetTcp->pkOnRecvDataFunc = pkOnRecvDataFunc;
+	pkNetTcp->kHandle = epoll_create(MAX_SOCKET_LEN);
+}
+
+#endif
--- magicnet/SeNetTcpForWin.c
+++ magicnet/SeNetTcpForWin.c
@@ -1,3 +1,14 @@
 #include "SeNetTcp.h"
 
+#if (defined(_WIN32) || defined(WIN32))
 
+void SeNetTcpInit(struct SENETTCP *pkNetTcp, SENETTCPONCONNECT pkOnConnectFunc, SENETTCPDISCONNECT pkOnDisconnectFunc, SENETTCPRECV pkOnRecvDataFunc)
+{
+	SeNetTcpCreate(pkNetTcp);
+	pkNetTcp->pkOnConnectFunc = pkOnConnectFunc;
+	pkNetTcp->pkOnDisconnectFunc = pkOnDisconnectFunc;
+	pkNetTcp->pkOnRecvDataFunc = pkOnRecvDataFunc;
+	pkNetTcp->kHandle = CreateIoCompletionPort(INVALID_HANDLE_VALUE, NULL, 0, 0);
+}
+
+#endif
