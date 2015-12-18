bb206965d29f95c3f330a49edb8f627984785a75
huangyacong <huangyacong8@163.com>
2015-12-18 19:18:57 +0800

    对齐 hash

--- common/SeHash.h
+++ common/SeHash.h
@@ -1,18 +1,22 @@
 #ifndef __SE_HASH_H__
 #define __SE_HASH_H__
 
+#include "SeTool.h"
 #include "SeList.h"
 #include <stdbool.h>
 
-struct SEHASHNODE
+// 64 ¶ÔÆëËü
+struct SE_ALIGN(64) SEHASHNODE
 {
 	struct SENODE main;
 	struct SENODE list;
 	int id;
 };
 
+// 32 size
 struct SEHASH
 {
+	int flag;
 	int max;
 	struct SELIST *pkMain;
 	struct SELIST list;
