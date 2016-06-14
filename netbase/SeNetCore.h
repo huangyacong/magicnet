2c49806c6240675fd82f6b0a8fd5a0a3d0db3b68
聪聪 <huangyacong8@163.com>
2016-06-14 20:33:42 +0800

    增加一个函数指针

--- netbase/SeNetCore.h
+++ netbase/SeNetCore.h
@@ -33,6 +33,8 @@ struct SENETCORE
 #endif
 };
 
+typedef void (*SENETCORERECV)(int iEvent, HSOCKET kHSocket, const char *pcBuf, int iLen, int iSendSize, int iRecvSize);
+
 void SeNetCoreInit(struct SENETCORE *pkNetCore, char *pcLogName, int iTimeOut, unsigned short usMax);
 
 void SeNetCoreFin(struct SENETCORE *pkNetCore);
