2a07b6c15ee9ad02b91042d4dee6e70c96d24c86
聪聪 <huangyacong8@163.com>
2020-02-07 23:54:04 +0800

    添加代理模式

---
+++ example/engine-lua/luabin/lib/AgentGate.lua
@@ -0,0 +1,178 @@
+﻿local IServerPlayer = require "IServerPlayer"
+local net_module = require "ccorenet"
+local CoreTool = require "CoreTool"
+local IServer = require "IServer"
+local util = require "util"
+require "class"
+
+local IAgentGateNetFunc_OnLocalRecvCall = "OnLocalRecvCall"
+local IAgentGateNetFunc_OnLocalRecvCommon = "OnLocalRecvCommon"
+local IAgentGateNetFunc_OnRemoteRecv = "OnRemoteRecv"
+local IAgentGateNetFunc_OnRemoteConnect = "OnRemoteConnect"
+local IAgentGateNetFunc_OnRemoteDisConnect = "OnRemoteDisConnect"
+
+local AgentGate = {}
+
+local AgentGateEvent = {}
+local AgentGateClassName = ""
+
+local AgentGateRemoteSvrObj = nil
+local AgentGateLocalSvrIPObj = nil
+local AgentGateLocalSvrUnixObj = nil
+
+-- 服务注册列表
+local AgentGateRegSvrList = {}
+
+-----------------------------------------------------------------
+local ServerRemotEvent = {}
+
+function ServerRemotEvent.OnConnect(IClientObj, socket, ip)
+	AgentGateEvent[IAgentGateNetFunc_OnRemoteConnect](socket, ip)
+end
+
+function ServerRemotEvent.OnDisConnect(IClientObj, socket)
+	AgentGateEvent[IAgentGateNetFunc_OnRemoteDisConnect](socket)
+end
+
+function ServerRemotEvent.OnRecv(IClientObj, socket, proto, data)
+	AgentGateEvent[IAgentGateNetFunc_OnRemoteRecv](socket, proto, data)
+end
+-----------------------------------------------------------------
+
+
+-----------------------------------------------------------------
+local ServerLocalEvent = {}
+
+function ServerLocalEvent.OnRecvCall(IServerObj, socket, targetName, proto, data)
+	if targetName == AgentGateClassName then
+		AgentGateEvent[IAgentGateNetFunc_OnLocalRecvCall](IServerObj, socket, proto, data)
+		return
+	end
+	if not AgentGateRegSvrList[targetName] then
+		print(debug.traceback(), "\n", string.format("AgentGate.OnRecvCommon not find targetName=%s error", targetName))
+		return
+	end
+	local IServerObj_, socket_ = table.unpack(AgentGateRegSvrList[targetName])
+	local ret, sendData = IServerObj_:CallData(socket_, targetName, proto, data)
+	if ret then
+		IServerObj:RetCallData(socket, sendData)
+	end
+end
+
+function ServerLocalEvent.OnRecvCommon(IServerObj, socket, targetName, proto, data)
+	if targetName == AgentGateClassName then
+		AgentGateEvent[IAgentGateNetFunc_OnLocalRecvCommon](proto, data)
+		return
+	end
+	if not AgentGateRegSvrList[targetName] then
+		print(debug.traceback(), "\n", string.format("AgentGate.OnRecvCommon not find targetName=%s error", targetName))
+		return
+	end
+	local IServerObj_, socket_ = table.unpack(AgentGateRegSvrList[targetName])
+	IServerObj_:SendData(socket_, targetName, proto, data)
+end
+
+function ServerLocalEvent.OnRecvRemote(IServerObj, socket, remote_socket, proto, data)
+	AgentGateRemoteSvrObj:SendData(remote_socket, proto, data)
+end
+
+function ServerLocalEvent.OnConnect(IServerObj, socket, ip)
+end
+
+function ServerLocalEvent.OnDisConnect(IServerObj, socket)
+	local name = IServerObj:GetSocketRegName(socket)
+	if name then
+		local _, socket_ = table.unpack(AgentGateRegSvrList[name])
+		if socket_ == socket then
+			AgentGateRegSvrList[name] = nil
+			print(string.format("AgentGate.OnRegister unregister name=%s ok", name))
+		end
+	end
+end
+
+function ServerLocalEvent.OnRegister(IServerObj, socket, name)
+	if IServerObj:GetSocketRegName(socket) ~= name then
+		IServerObj:DisConnect(socket)
+		print(debug.traceback(), "\n", string.format("AgentGate.OnRegister name=%s error", name))
+		return
+	end
+	if AgentGateRegSvrList[name] or AgentGateClassName == name then
+		IServerObj:DisConnect(socket)
+		print(debug.traceback(), "\n", string.format("AgentGate.OnRegister name=%s AgentGateClassName=%s is same", name, AgentGateClassName))
+		return
+	end
+	AgentGateRegSvrList[name] = table.pack(IServerObj, socket)
+	print(string.format("AgentGate.OnRegister register name=%s ok", name))
+end
+-----------------------------------------------------------------
+
+function AgentGate.SendRemote(remote_socket, proto, data)
+	return AgentGateRemoteSvrObj:SendData(remote_socket, proto, data)
+end
+
+function AgentGate.SendData(targetName, proto, data)
+	if not AgentGateRegSvrList[targetName] then
+		print(debug.traceback(), "\n", string.format("AgentGate.SendData not find targetName=%s error", targetName))
+		return false
+	end
+	local IServerObj_, socket_ = table.unpack(AgentGateRegSvrList[targetName])
+	return IServerObj_:SendData(socket_, targetName, proto, data)
+end
+
+function AgentGate:CallData(targetName, proto, data, timeout_millsec)
+	if not AgentGateRegSvrList[targetName] then
+		print(debug.traceback(), "\n", string.format("AgentGate.CallData not find targetName=%s error", targetName))
+		return false, "send failed"
+	end
+	local IServerObj_, socket_ = table.unpack(AgentGateRegSvrList[targetName])
+	return IServerObj_:CallData(socket_, targetName, proto, data, timeout_millsec)
+end
+
+function AgentGate.Init(className, modulename, cRemoteIP, iRemotePort, iRemoteTimeOut, cLocalIP, iLocalPort, cUnixSocketName, iLocalTimeOut, bReusePort, bNoDelay)
+	-- 模块modulename中必须是table，同时必须有下面的key
+
+	if type(modulename) ~= type({}) then
+		print(debug.traceback(), "\n", "AgentGate.Init modulename not a table")
+		return false
+	end
+
+	if not next(modulename) then
+		print(debug.traceback(), "\n", string.format("AgentGate.Init modulename is empty"))
+		return false
+	end
+
+	local funtList = {IAgentGateNetFunc_OnLocalRecvCall, IAgentGateNetFunc_OnLocalRecvCommon, IAgentGateNetFunc_OnRemoteRecv, IAgentGateNetFunc_OnRemoteConnect, IAgentGateNetFunc_OnRemoteDisConnect}
+	for _, funtname in pairs(funtList) do
+		if not modulename[funtname] then
+			print(string.format(debug.traceback(), "\n", "AgentGate.Init modulename not has key=%s", funtname))
+			return false
+		end
+	end
+
+	if ccorenet.getOS() == "Linux" then
+		AgentGateLocalSvrUnixObj = IServer.new("UnixLocal-World", ServerLocalEvent, cUnixSocketName, 0, iLocalTimeOut, net_module.UnixLocal, false, bNoDelay)
+		if not AgentGateLocalSvrUnixObj:Listen() then
+			print(debug.traceback(), "\n", string.format("AgentGate.Init %s Listen Failed. cLocalIP=%s iLocalPort=%s", AgentGateLocalSvrUnixObj:GetName(), cUnixSocketName, 0))
+			return false
+		end
+	end
+
+	AgentGateLocalSvrIPObj = IServer.new("IpV4-World", ServerLocalEvent, cLocalIP, iLocalPort, iLocalTimeOut, net_module.IpV4, false, bNoDelay)
+	if not AgentGateLocalSvrIPObj:Listen() then
+		print(debug.traceback(), "\n", string.format("AgentGate.Init %s Listen Failed. cLocalIP=%s iLocalPort=%s", AgentGateLocalSvrIPObj:GetName(), cLocalIP, iLocalPort))
+		return false
+	end
+
+	AgentGateRemoteSvrObj = IServerPlayer.new("IpV4-player", ServerRemotEvent, cRemoteIP, iRemotePort, iRemoteTimeOut, net_module.IpV4, bReusePort, bNoDelay)
+	if not AgentGateRemoteSvrObj:Listen() then
+		print(debug.traceback(), "\n", string.format("AgentGate.Init %s Listen Failed. cLocalIP=%s iLocalPort=%s", AgentGateRemoteSvrObj:GetName(), cRemoteIP, iRemotePort))
+		return false
+	end
+
+	-- 赋值
+	AgentGateEvent = modulename
+	AgentGateClassName = className
+	return true
+end
+
+return util.ReadOnlyTable(AgentGate)
---
+++ example/engine-lua/luabin/lib/AgentService.lua
@@ -0,0 +1,97 @@
+﻿local net_module = require "ccorenet"
+local CoreTool = require "CoreTool"
+local IClient = require "IClient"
+local util = require "util"
+require "class"
+
+local IAgentServiceNetFunc_OnRecvCall = "OnRecvCall"
+local IAgentServiceNetFunc_OnRecvCommon = "OnRecvCommon"
+
+local AgentService = {}
+
+local AgentServiceEvent = {}
+local AgentServiceClassName = ""
+
+local AgentServiceRemoteSvrObj = nil
+
+-----------------------------------------------------------------
+local ServerClientEvent = {}
+
+function ServerClientEvent.OnRecvCall(IClientObj, targetName, proto, data)
+	AgentGateEvent[IAgentServiceNetFunc_OnRecvCall](proto, data)
+end
+
+function ServerClientEvent.OnRecvCommon(IClientObj, targetName, proto, data)
+	AgentGateEvent[IAgentServiceNetFunc_OnRecvCommon](proto, data)
+end
+
+function ServerClientEvent.OnConnect(IClientObj, ip)
+end
+
+function ServerClientEvent.OnDisConnect(IClientObj)
+end
+
+function ServerClientEvent.OnConnectFailed(IClientObj)
+end
+-----------------------------------------------------------------
+
+function AgentService.SendRemote(remote_socket, proto, data)
+	return AgentServiceRemoteSvrObj:SendRemoteData(remote_socket, proto, data)
+end
+
+function AgentService.SendData(targetName, proto, data)
+	return AgentServiceRemoteSvrObj:SendData(targetName, proto, data)
+end
+
+function AgentService:CallData(targetName, proto, data, timeout_millsec)
+	return AgentServiceRemoteSvrObj:CallData(targetName, proto, data, timeout_millsec)
+end
+
+function AgentService.RetCallData(data)
+	return AgentServiceRemoteSvrObj:RetCallData(data)
+end
+
+function AgentService.Init(className, modulename, cRemoteIP, iRemotePort, cUnixSocketName, iTimeOut, iConnectTimeOut, bNoDelay)
+	-- 模块modulename中必须是table，同时必须有下面的key
+
+	if type(modulename) ~= type({}) then
+		print(debug.traceback(), "\n", "AgentService.Init modulename not a table")
+		return false
+	end
+
+	if not next(modulename) then
+		print(debug.traceback(), "\n", string.format("AgentService.Init modulename is empty"))
+		return false
+	end
+
+	local funtList = {IAgentServiceNetFunc_OnRecvCall, IAgentServiceNetFunc_OnRecvCommon}
+	for _, funtname in pairs(funtList) do
+		if not modulename[funtname] then
+			print(string.format(debug.traceback(), "\n", "AgentService.Init modulename not has key=%s", funtname))
+			return false
+		end
+	end
+
+	local iDomain = (ccorenet.getOS() == "Linux") and net_module.UnixLocal or net_module.IpV4
+
+	if iDomain == net_module.IpV4 then
+		AgentServiceRemoteSvrObj = IClient.new("IpV4-World", ServerClientEvent, cRemoteIP, iRemotePort, iTimeOut, iConnectTimeOut, iDomain, bNoDelay)
+	elseif iDomain == net_module.UnixLocal then
+		AgentServiceRemoteSvrObj = IClient.new("UnixLocal-World", ServerClientEvent, cUnixSocketName, 0, iTimeOut, iConnectTimeOut, iDomain, bNoDelay)
+	else
+		print(string.format(debug.traceback(), "\n", "AgentService.Init iDomain=%s error", iDomain))
+		return false
+	end
+
+	if not AgentServiceRemoteSvrObj:Connect() then
+		print(debug.traceback(), "\n", string.format("AgentService.Connect %s Listen Failed. cLocalIP=%s iLocalPort=%s", AgentGateRemoteSvrObj:GetName(), cRemoteIP, iRemotePort))
+		return false
+	end
+
+	-- 赋值
+	AgentServiceEvent = modulename
+	AgentServiceClassName = className
+	return true
+end
+
+return util.ReadOnlyTable(AgentService)
