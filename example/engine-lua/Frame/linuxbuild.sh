dc8567447391768c259bc67cb2ee0af3bce2e35d
聪聪 <huangyacong8@163.com>
2020-02-15 12:16:17 +0800

    路径

--- example/engine-lua/Frame/linuxbuild.sh
+++ example/engine-lua/Frame/linuxbuild.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-cp -rf ..\..\..\common\* .\src\CoreSrc\common\
-cp -rf ..\..\..\netbase\* .\src\CoreSrc\netbase\
-cp -rf ..\..\..\mysql\* .\src\CoreSrc\mysql\
-cp -rf ..\..\..\cpp\* .\src\NetEngine\
+cp -rf ../../../common/* ./src/CoreSrc/common/
+cp -rf ../../../netbase/* ./src/CoreSrc/netbase/
+cp -rf ../../../mysql/* ./src/CoreSrc/mysql/
+cp -rf ../../../cpp/* ./src/NetEngine/
 
 cd CoreLog
 make clean
@@ -26,12 +26,12 @@ rm -rf *.o
 make
 cd ..
 
-rm -rf .\src\CoreSrc\mysql\dev
-rm -r .\src\CoreSrc\common\*.h
-rm -r .\src\CoreSrc\common\*.c
-rm -r .\src\CoreSrc\netbase\*.h
-rm -r .\src\CoreSrc\netbase\*.c
-rm -r .\src\CoreSrc\mysql\*.h
-rm -r .\src\CoreSrc\mysql\*.c
-rm -r .\src\NetEngine\*.h
-rm -r .\src\NetEngine\*.cpp
+rm -rf ./src/CoreSrc/mysql/dev
+rm -r ./src/CoreSrc/common/*.h
+rm -r ./src/CoreSrc/common/*.c
+rm -r ./src/CoreSrc/netbase/*.h
+rm -r ./src/CoreSrc/netbase/*.c
+rm -r ./src/CoreSrc/mysql/*.h
+rm -r ./src/CoreSrc/mysql/*.c
+rm -r ./src/NetEngine/*.h
+rm -r ./src/NetEngine/*.cpp
