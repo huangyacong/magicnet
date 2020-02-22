5788a2bb9993a874dbdb78ef5c42405ba4b874c6
admin-PC\admin <huangyacong8@163.com>
2020-02-22 16:56:38 +0800

    感知table发生变化

--- example/engine-lua/luabin/lib/util.lua
+++ example/engine-lua/luabin/lib/util.lua
@@ -132,12 +132,16 @@ end
 
 local keyWords = {["__bUpdate"] = true, ["__iSessionId"] = true}
 
-function util.AddTableAutoUpdateMsg(t, keyWordsArray)
+function util.AddTableAutoUpdateMsg(t, bchange, keyWordsArray)
 	for k, v in pairs(t) do
 		assert(not keyWords[k], string.format("Class Has Key=%s, this can only use in system!", k))
 	end
 	for _, k in ipairs(keyWordsArray or {}) do
 		keyWords[k] = true
+	end
+	if bchange then
+		t.__bUpdate = true
+		t.__iSessionId = CoreTool.SysSessionId()
 	end
 	local proxy = {}
 	local mt = {
