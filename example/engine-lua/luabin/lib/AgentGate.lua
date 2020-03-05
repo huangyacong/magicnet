df1332c763e05da6a146bde467e6269275624bf1
admin-PC\admin <huangyacong8@163.com>
2020-03-05 17:23:04 +0800

    名字出错

--- example/engine-lua/luabin/lib/AgentGate.lua
+++ example/engine-lua/luabin/lib/AgentGate.lua
@@ -1,22 +1,22 @@
-﻿local net_module = require "ccorenet"
+﻿local CoreNet = require "CoreNet"
 local util = require "util"
 
 local AgentGate = {}
 
 function AgentGate.Init(cLogName, iMaxClientNum, bPrintLog2Screen)
-	net_module.InitAgentGate(cLogName, iMaxClientNum, 16, bPrintLog2Screen)
+	CoreNet.InitAgentGate(cLogName, iMaxClientNum, 16, bPrintLog2Screen)
 end
 
 function AgentGate.Listen(IPRemote, PortRemote, IPService, PortService, IPServiceUnix, iTimeOut)
-	return net_module.AgentGateListen(IPRemote, PortRemote, IPService, PortService, IPServiceUnix, iTimeOut)
+	return CoreNet.AgentGateListen(IPRemote, PortRemote, IPService, PortService, IPServiceUnix, iTimeOut)
 end
 
 function AgentGate.Start()
-	return net_module.StartAgentGate()
+	return CoreNet.StartAgentGate()
 end
 
 function AgentGate.Fin()
-	net_module.FinAgentGate()
+	CoreNet.FinAgentGate()
 end
 
 return util.ReadOnlyTable(AgentGate)
