17eaff62b77e77c61cf0fde1ec5ecd7de11d2949
admin-PC\admin <huangyacong8@163.com>
2020-03-06 16:28:09 +0800

    修改build文件

--- example/engine-lua/Frame/linuxbuild.sh
+++ example/engine-lua/Frame/linuxbuild.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+
+cp -rf ../../../common/* ./src/CoreSrc/common/
+cp -rf ../../../netbase/* ./src/CoreSrc/netbase/
+cp -rf ../../../mysql/* ./src/CoreSrc/mysql/
+cp -rf ../../../cpp/* ./src/NetEngine/
+
 cd CoreLog
 make clean
 make
@@ -23,4 +29,14 @@ cd protobuf
 rm -rf *.o
 make
 cd ..
+
+rm -rf ./src/CoreSrc/mysql/dev
+rm -r ./src/CoreSrc/common/*.h
+rm -r ./src/CoreSrc/common/*.c
+rm -r ./src/CoreSrc/netbase/*.h
+rm -r ./src/CoreSrc/netbase/*.c
+rm -r ./src/CoreSrc/mysql/*.h
+rm -r ./src/CoreSrc/mysql/*.c
+rm -r ./src/NetEngine/*.h
+rm -r ./src/NetEngine/*.cpp
 
