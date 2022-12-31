be0273bc08ce92c308c682afea6f93e1326ebb79
聪聪 <huangyacong8@163.com>
2022-12-31 11:14:29 +0800

    test

--- example/engine-lua/luabin/logic/loggate.lua
+++ example/engine-lua/luabin/logic/loggate.lua
@@ -5,7 +5,7 @@ local timer = require "timer"
 
 AgentGate.Init("./test_svr_gate.log", 65535, true)
 
-AgentGate.Listen("127.0.0.1", 8168, "127.0.0.1", 6666, "sssssss", 60000)
+AgentGate.Listen("127.0.0.1", 8888, "127.0.0.1", 6666, "sssssss", 60000)
 
 AgentGate.Start()
 
